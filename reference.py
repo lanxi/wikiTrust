17b151264f136d3bf4059990a76545778cc113cf
lanxi <lanxi@users.noreply.github.com>
2015-03-27 09:26:32 -0400

    query reference measurement from Google Scholar

--- reference.py
+++ reference.py
@@ -1,26 +1,61 @@
-from lxml import html
 import requests
+from bs4 import BeautifulSoup
 
-baseURL = 'https://scholar.google.com'
-# the DOI/ISBN for this reference
-# parse the reference number
-relativeURL = '/scholar?q='+'################'
-reference_page = requests.get(baseURL+relativeURL)
-tree = html.fromstring(reference_page)
-
-title = tree.xpath('//div[@id="gs_ccl"]/div[1]//h3[@class="gs_rt"]/text()')
-# author links
-author = tree.xpath('//div[@id="gs_ccl"]/div[1]//h3[@class="gs_a"]/a/@href')
-# number of citations
-citation = tree.xpath('//div[@id="gs_ccl"]/div[1]//h3[@class="gs_fl"]/a[1]/text()')
-citation_string = citation[0]
-number = [int(s) for s in citation_string.split() if s.isdigit()]
-citation_number = number[0]
-
-# author h-index
-for author_URL in author:
-	author_page = requests.get(baseURL+authorURL)
-	tree = html.fromstring(author_page)
-	h = tree.xpath('//table[@id="gsc_rsb_st"]//tr[3]/td[@class="gsc_rsb_std"]/text()')
-	h_all = h[0]
-	h_2010 = h[1]
+def ref_quality(refID):
+ quality = 0
+ baseURL = 'https://scholar.google.com'
+ # the DOI/ISBN for this reference
+ # parse the reference number
+ relativeURL = '/scholar?q='+ refID
+ reference_page = requests.get(baseURL+relativeURL, verify=False)
+ soup = BeautifulSoup(reference_page.text)
+ 
+ # title
+ title = soup.find("h3", {"class": "gs_rt"}).text
+ # author links
+ authors = test=soup.find("div", {"class": "gs_a"})
+ author_ref = []
+ for a in authors.find_all('a', href=True):
+  author_ref.append(a['href'])
+ 
+ # number of citations
+ gs_fl = soup.find_all("div", {"class": "gs_fl"})
+ citation = None
+ for c in gs_fl:
+  if c['class'] == ['gs_fl']:
+   citation = c.find('a', href=True).text
+   break
+ 
+ if bool(citation) == True:
+  citation_num = [int(s) for s in citation.split() if s.isdigit()][0]
+  quality += citation_num
+ else:
+  print ('no number of citation found')
+ 
+ # author h-index
+ user_agent = {'User-agent': 'Mozilla/5.0'}
+ if bool(author_ref) == True:
+  for a in author_ref:
+   author_page = requests.get(baseURL+a, verify=False, headers=user_agent)
+   #print (author_page)
+   soup = BeautifulSoup(author_page.text)
+   index = soup.find_all("td",{"class": "gsc_rsb_std"})
+   indexes = []
+   for i in index:
+    indexes.append(i.text)
+   h_index = None
+   h_index_2010 = None
+   if bool(indexes) == True:
+    h_index = int(indexes[2].encode("ascii"))
+    h_index_2010 = int(indexes[3].encode("ascii"))
+    quality += h_index
+   else:
+    print ('no h-index found')
+ else:
+  print ('no author reference link found')
+  #tree = html.fromstring(author_page)
+  #h = tree.xpath('//table[@id="gsc_rsb_st"]//tr[3]/td[@class="gsc_rsb_std"]/text()')
+  #h_all = h[0]
+  #h_2010 = h[1]
+ 
+ return quality
